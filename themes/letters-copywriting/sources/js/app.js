2f06ce8d8064fa67dab86adefc61c68398fd4f70
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-06-01 19:18:18 +0430

    Initial Commit

---
+++ themes/letters-copywriting/sources/js/app.js
@@ -0,0 +1,8 @@
+$(function() { // Shorthand for $( document ).ready()
+    "use strict";
+    // Your js script is below this line
+    // --------------------------------------------------------------------- //
+
+    // Example
+    console.log( "ready!" );
+});
