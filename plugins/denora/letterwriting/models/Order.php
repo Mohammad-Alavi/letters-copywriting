8c11a870dcba379c1dfa048927e7ccfc508f7543
Moslem Deris <moslem.deris@gmail.com>
2019-06-03 02:51:29 +0430

    Add `Order` model to plugin `LetterWriting`

---
+++ plugins/denora/letterwriting/models/Order.php
@@ -0,0 +1,27 @@
+<?php namespace Denora\Letterwriting\Models;
+
+use Model;
+
+/**
+ * Model
+ */
+class Order extends Model
+{
+    use \October\Rain\Database\Traits\Validation;
+    
+    use \October\Rain\Database\Traits\SoftDelete;
+
+    protected $dates = ['deleted_at'];
+
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'denora_letterwriting_orders';
+
+    /**
+     * @var array Validation rules
+     */
+    public $rules = [
+    ];
+}
