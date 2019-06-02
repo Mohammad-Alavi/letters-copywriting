521636cf0c5cb8383aa57a1443ee0ac4a653c061
Moslem Deris <moslem.deris@gmail.com>
2019-06-03 02:24:50 +0430

    Add `Statuses` table to plugin `LetterWriting`

---
+++ plugins/denora/letterwriting/updates/builder_table_create_denora_letterwriting_statuses.php
@@ -0,0 +1,26 @@
+<?php namespace Denora\Letterwriting\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateDenoraLetterwritingStatuses extends Migration
+{
+    public function up()
+    {
+        Schema::create('denora_letterwriting_statuses', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('order_id');
+            $table->string('label');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('denora_letterwriting_statuses');
+    }
+}
