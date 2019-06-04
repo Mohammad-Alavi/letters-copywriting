296d9bdefd1385835d7d357814d7a2625e0261f0
Moslem Deris <moslem.deris@gmail.com>
2019-06-04 18:26:26 +0430

    Add `OrderList` component

--- plugins/denora/letterwriting/Plugin.php
+++ plugins/denora/letterwriting/Plugin.php
@@ -2,13 +2,25 @@
 
 use System\Classes\PluginBase;
 
-class Plugin extends PluginBase
-{
-    public function registerComponents()
-    {
+class Plugin extends PluginBase {
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents() {
+        return [
+            'Denora\Letterwriting\Components\OrderList'    => 'orderList',
+        ];
     }
 
-    public function registerSettings()
-    {
+    /**
+     * Registers any back-end configuration links used by this plugin.
+     *
+     * @return array
+     */
+    public function registerSettings() {
+
     }
 }
