d69aedf6f2dd4300f8b923cd949190d581a6ef56
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-06-14 07:21:08 +0430

    add ajax seach-field for order-list

--- themes/letters-copywriting/sources/js/app.js
+++ themes/letters-copywriting/sources/js/app.js
@@ -5,4 +5,15 @@ $(function() { // Shorthand for $( document ).ready()
 
     // Example
     console.log( "ready!" );
+
+    // search functionality in order-list page
+    $(document).ready(function(){
+        $("#myInput").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $("#order-list-tbody tr").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
+    });
 });
+
