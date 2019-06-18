0056da413b35fc09a6a77fd58568d28cb330e1ea
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2019-06-18 05:59:27 +0430

    refactor

--- themes/letters-copywriting/sources/js/app.js
+++ themes/letters-copywriting/sources/js/app.js
@@ -7,13 +7,10 @@ $(function() { // Shorthand for $( document ).ready()
     console.log( "ready!" );
 
     // search functionality in order-list page
-    $(document).ready(function(){
-        $("#myInput").on("keyup", function() {
-            var value = $(this).val().toLowerCase();
-            $("#order-list-tbody tr").filter(function() {
-                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-            });
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#order-list-tbody tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
 });
-
